f38e403ca1c222a7376eaf12b2a3a4e696dd1801
Bjorn Sandvik <bjorn@mastermaps.com>
2016-01-08 11:38:49 +0100

    Bug fix

--- src/core/LayerHandlerFacility.js
+++ src/core/LayerHandlerFacility.js
@@ -177,8 +177,6 @@ GIS.core.LayerHandlerFacility = function(gis, layer) {
 				return;
 			}
 
-
-
 			// Store features for search
 			//layer.featureStore.loadFeatures(features.slice(0));
             layer.featureStore.loadFeatures(features);

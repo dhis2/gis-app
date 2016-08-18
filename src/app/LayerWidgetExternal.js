ad6eafc76623e2d376ecaeffa75ced08edaf488f
Bjorn Sandvik <bjorn@mastermaps.com>
2016-08-18 14:19:36 +0200

    Basic WMS/TMS/XYZ layer handling

--- src/app/LayerWidgetExternal.js
+++ src/app/LayerWidgetExternal.js
@@ -1,5 +1,63 @@
 export default function LayerWidgetExternal(gis, layer) {
 
+    // Combo with with supported Earth Engine layers
+    const serviceCombo = Ext.create('Ext.form.field.ComboBox', {
+        cls: 'gis-combo',
+        // fieldLabel: GIS.i18n.select_layer_from_google_earth_engine,
+        fieldLabel: 'Select service', // TODO: i18n
+        editable: false,
+        valueField: 'id',
+        displayField: 'name',
+        queryMode: 'local',
+        forceSelection: true,
+        labelWidth: gis.conf.layout.widget.itemlabel_width,
+        width: gis.conf.layout.widget.item_width,
+        store: Ext.create('Ext.data.Store', {
+            fields: ['id', 'name'],
+            data: [{
+                id: 'wms',
+                name: 'WMS'
+            },{
+                id: 'tms',
+                name: 'TMS'
+            },{
+                id: 'xyz',
+                name: 'XYZ'
+            }],
+        }),
+        listeners: {
+            render() { // Select first record (WMS)
+                this.select(this.getStore().getAt(2));
+            },
+            change() { // Show/hide WMS layers field
+                layersField[this.getValue() === 'wms' ? 'show' : 'hide']();
+            }
+        }
+    });
+
+    const urlField = Ext.create('Ext.form.field.Text', {
+        cls: 'gis-combo',
+        fieldLabel: 'Service URL', // TODO: i18n
+        labelWidth: gis.conf.layout.widget.itemlabel_width,
+        width: gis.conf.layout.widget.item_width
+    });
+
+    const layersField = Ext.create('Ext.form.field.Text', {
+        cls: 'gis-combo',
+        fieldLabel: 'Layers', // TODO: i18n
+        labelWidth: gis.conf.layout.widget.itemlabel_width,
+        width: gis.conf.layout.widget.item_width,
+        hidden: true
+    });
+
+    const attributionField = Ext.create('Ext.form.field.Text', {
+        cls: 'gis-combo',
+        fieldLabel: 'Attribution', // TODO: i18n
+        labelWidth: gis.conf.layout.widget.itemlabel_width,
+        width: gis.conf.layout.widget.item_width
+    });
+
+
     // Reset this widget
     const reset = function() {
 
@@ -12,7 +70,20 @@ export default function LayerWidgetExternal(gis, layer) {
 
     // Get the view representation of the layer
     const getView = function() {
-        const view = {};
+        const service = serviceCombo.getValue();
+        const url = urlField.getValue();
+        const layers = layersField.getValue();
+        const attribution = attributionField.getValue();
+
+        const view = {
+            service,
+            url,
+            attribution
+        };
+
+        if (layers) {
+            view.layers = layers;
+        }
 
         return view;
     };
@@ -20,7 +91,7 @@ export default function LayerWidgetExternal(gis, layer) {
     // Return widget panel
     return Ext.create('Ext.panel.Panel', {
         bodyStyle: 'border:0;padding:5px 1px;',
-        items: [],
+        items: [serviceCombo, urlField, layersField, attributionField],
         map: layer.map,
         layer: layer,
         menu: layer.menu,
--- src/core/LayerHandlerExternal.js
+++ src/core/LayerHandlerExternal.js
@@ -1,5 +1,48 @@
 export default function LayerHandlerExternal(gis, layer) {
 
+    const createLayer = function(view) {
+        const layerConfig = {
+            type: 'tileLayer',
+            url: view.url,
+            attribution: view.attribution
+        };
 
-    return true;
+        if (view.service === 'tms') {
+            layerConfig.tms = true;
+        }
+
+        if (view.service === 'wms') {
+            layerConfig.type = 'wmsLayer';
+            layerConfig.layers = view.layers;
+        }
+
+        // Remove layer instance if already exist
+        if (layer.instance && gis.instance.hasLayer(layer.instance)) {
+            gis.instance.removeLayer(layer.instance);
+        }
+
+        // Create layer instance
+        layer.instance = gis.instance.addLayer(layerConfig);
+
+        if (gis.mask && loader.hideMask) {
+            gis.mask.hide();
+        }
+    }
+
+    const loader = {
+        compare: false,
+        updateGui: false,
+        zoomToVisibleExtent: false,
+        hideMask: false,
+        callBack: null,
+        load: function(view) {
+            if (gis.mask && !gis.skipMask) {
+                gis.mask.show();
+            }
+
+            createLayer(view);
+        }
+    };
+
+    return loader;
 }

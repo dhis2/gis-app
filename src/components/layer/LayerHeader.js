3d99f10d9fc5051fea9ea48e5b4c10fbef80e91c
Bjørn Sandvik <bjorn@mastermaps.com>
2017-01-23 11:48:44 +0100

    Layer component

--- src/components/layer/LayerHeader.js
+++ src/components/layer/LayerHeader.js
@@ -1,52 +1,67 @@
 import React from 'react';
 import IconButton from 'material-ui/IconButton';
-import ActionVisibility from 'material-ui/svg-icons/action/visibility';
-import NavigationExpandMore from 'material-ui/svg-icons/navigation/expand-more';
+import ActionVisibilityIcon from 'material-ui/svg-icons/action/visibility';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import {grey600} from 'material-ui/styles/colors'; // http://www.material-ui.com/#/customization/colors
 
-
-function VerticalDragIcon() {
+function VerticalDragIcon(props) {
     return (
         <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
-            <path fill="#000000" d="M9,3H11V5H9V3M13,3H15V5H13V3M9,7H11V9H9V7M13,7H15V9H13V7M9,11H11V13H9V11M13,11H15V13H13V11M9,15H11V17H9V15M13,15H15V17H13V15M9,19H11V21H9V19M13,19H15V21H13V19Z" />
+            <path fill={props.color} d="M9,3H11V5H9V3M13,3H15V5H13V3M9,7H11V9H9V7M13,7H15V9H13V7M9,11H11V13H9V11M13,11H15V13H13V11M9,15H11V17H9V15M13,15H15V17H13V15M9,19H11V21H9V19M13,19H15V21H13V19Z" />
         </svg>
     );
 }
 
 const styles = {
+    header: {
+        backgroundColor: '#eee',
+        height: 48
+    },
     title: {
         display: 'inline',
         padding: 0,
         margin: 0,
+        fontWeight: 500,
+        fontFamily: 'Roboto, sans-serif',
         fontSize: 16,
-        color: '#333'
+        color: '#333',
+        lineHeight: '48px',
+        WebkitMarginBefore: 0,
+        WebkitMarginAfter: 0,
+        marginTop: -10
+    },
+    tools: {
+        float: 'right'
     }
 };
 
 export default function LayerWidget(props) {
     return (
-        <div>
+        <div style={styles.header}>
             <IconButton
                 onClick={(event) => props.onDragClicked(event, props.value)}
                 tooltip="Drag to reorder"
             >
-                <VerticalDragIcon />
+                <VerticalDragIcon color={grey600} />
             </IconButton>
 
             <h2 style={styles.title}>{props.title}</h2>
 
-            <IconButton
-                onClick={(event) => props.onVisibilityClicked(event, props.value)}
-                tooltip="Toggle visibility"
-            >
-                <ActionVisibility />
-            </IconButton>
+            <div style={styles.tools}>
+                <IconButton
+                    onClick={(event) => props.onVisibilityClicked(event, props.value)}
+                    tooltip="Toggle visibility"
+                >
+                    <ActionVisibilityIcon color={grey600} />
+                </IconButton>
 
-            <IconButton
-                onClick={(event) => props.onExpandClicked(event, props.value)}
-                tooltip="Expand"
-            >
-                <NavigationExpandMore />
-            </IconButton>
+                <IconButton
+                    onClick={(event) => props.onExpandClicked(event, props.value)}
+                    tooltip="Expand"
+                >
+                    <NavigationExpandMoreIcon color={grey600} />
+                </IconButton>
+            </div>
         </div>
     );
 }
--- src/components/layer/LayerToolbar.js
+++ src/components/layer/LayerToolbar.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import IconButton from 'material-ui/IconButton';
+import ContentCreateIcon from 'material-ui/svg-icons/action/visibility';
+import ActionFilterListIcon from 'material-ui/svg-icons/content/filter-list';
+import ActionSearchIcon from 'material-ui/svg-icons/action/search';
+import ActionDeleteIcon from 'material-ui/svg-icons/action/delete';
+import Slider from 'material-ui/Slider';
+import {grey600} from 'material-ui/styles/colors'; // http://www.material-ui.com/#/customization/colors
+
+const styles = {
+    toolbar: {
+        backgroundColor: '#eee',
+        height: 36,
+        borderBottom: '1px solid #aaa'
+    },
+    button: {
+        float: 'left',
+        padding: 6,
+        width: 36,
+        height: 36,
+    },
+    rightButton: {
+        float: 'right',
+        padding: 6
+    },
+    slider: {
+        float: 'left',
+        width: 100
+    }
+};
 
 export default function LayerToolbar() {
     return (
-        <div>Layer toolbar</div>
+        <div style={styles.toolbar}>
+            <IconButton tooltip="Edit" style={styles.button}>
+                <ContentCreateIcon color={grey600} />
+            </IconButton>
+            <IconButton tooltip="Search" style={styles.button}>
+                <ActionSearchIcon color={grey600} />
+            </IconButton>
+            <IconButton tooltip="Filter" style={styles.button}>
+                <ActionFilterListIcon color={grey600} />
+            </IconButton>
+            <Slider defaultValue={0.9} style={styles.slider} />
+            <IconButton tooltip="Delete" style={styles.button}>
+                <ActionDeleteIcon color={grey600} />
+            </IconButton>
+        </div>
     );
 }

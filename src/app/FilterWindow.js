29be6bf9da8c4cd3a0c82e220ee4dbc7e4a17581
Bjørn Sandvik <bjorn@mastermaps.com>
2017-01-26 14:40:43 +0100

    Filter code clean

--- src/app/FilterWindow.js
+++ src/app/FilterWindow.js
@@ -1,45 +1,41 @@
 export default function FilterWindow(gis, layer) {
-    var lowerNumberField,
-        greaterNumberField,
-        lt,
-        gt,
-        filter,
-        window;
+    let lt;
+    let gt;
 
-    greaterNumberField = Ext.create('Ext.form.field.Number', {
+    const greaterNumberField = Ext.create('Ext.form.field.Number', {
         cls: 'gis-numberfield',
         fieldLabel: GIS.i18n.greater_than,
         width: 200,
         value: layer.minValue,
         listeners: {
-            change: function() {
+            change() {
                 gt = this.getValue();
             }
         }
     });
 
-    lowerNumberField = Ext.create('Ext.form.field.Number', {
+    const lowerNumberField = Ext.create('Ext.form.field.Number', {
         cls: 'gis-numberfield',
         fieldLabel: GIS.i18n.and_or_lower_than,
         style: 'margin-bottom: 0',
         width: 200,
         value: layer.maxValue + 1,
         listeners: {
-            change: function() {
+            change() {
                 lt = this.getValue();
             }
         }
     });
 
-    filter = function() {
-        var cache = layer.featureStore.features.slice(0),
-            features = [];
+    const filter = function() {
+        const cache = layer.featureStore.features.slice(0);
+        let features = [];
 
         if (!gt && !lt) {
             features = cache;
         }
         else if (gt && lt) {
-            for (var i = 0; i < cache.length; i++) {
+            for (let i = 0; i < cache.length; i++) {
                 if (gt < lt && (cache[i].properties.value > gt && cache[i].properties.value < lt)) {
                     features.push(cache[i]);
                 }
@@ -52,14 +48,14 @@ export default function FilterWindow(gis, layer) {
             }
         }
         else if (gt && !lt) {
-            for (var i = 0; i < cache.length; i++) {
+            for (let i = 0; i < cache.length; i++) {
                 if (cache[i].properties.value > gt) {
                     features.push(cache[i]);
                 }
             }
         }
         else if (!gt && lt) {
-            for (var i = 0; i < cache.length; i++) {
+            for (let i = 0; i < cache.length; i++) {
                 if (cache[i].properties.value < lt) {
                     features.push(cache[i]);
                 }
@@ -70,7 +66,7 @@ export default function FilterWindow(gis, layer) {
         layer.instance.addData(features);
     };
 
-    window = Ext.create('Ext.window.Window', {
+    const window = Ext.create('Ext.window.Window', {
         title: GIS.i18n.filter_by_value,
         iconCls: 'gis-window-title-icon-filter',
         bodyStyle: 'background-color: #fff; padding: 1px',
@@ -94,16 +90,15 @@ export default function FilterWindow(gis, layer) {
             {
                 xtype: 'button',
                 text: GIS.i18n.update,
-                handler: function() {
-                    filter();
-                }
+                handler: filter
+
             }
         ],
         listeners: {
-            render: function() {
+            render() {
                 gis.util.gui.window.setPositionTopLeft(this);
             },
-            destroy: function() {
+            destroy() {
                 layer.instance.clearLayers();
                 layer.instance.addData(layer.featureStore.features);
             }

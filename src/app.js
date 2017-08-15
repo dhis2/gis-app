23dca07dce7be898351f90d7593ea00653eeaece
Bjørn Sandvik <bjorn@mastermaps.com>
2017-08-15 02:25:30 +0200

    IE11 api fix

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import arrayPluck from 'd2-utilizr/lib/arrayPluck';
 import log from 'loglevel';
 
 log.setLevel(log.levels.INFO); // TODO: Use DEBUG for development
-log.info('Loading: GIS app 28.0.1'); // TODO: Use manifest
+log.info('Loading: GIS app 28.0.2'); // TODO: Use manifest
 
 window.GIS = GIS;
 

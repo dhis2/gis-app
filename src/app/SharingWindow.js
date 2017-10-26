31387a8bb652aa949977910b3dc0648661e8232c
Bjørn Sandvik <bjorn@mastermaps.com>
2017-10-26 11:56:24 +0200

    Sharing bug fix

--- src/app/SharingWindow.js
+++ src/app/SharingWindow.js
@@ -179,8 +179,8 @@ export default function SharingWindow(gis, sharing) {
         handler: function(b) {
             var id = userGroupField.getValue(),
                 name = userGroupField.getRawValue(),
-                userGroupAccesses = getBody().object.userGroupAccesses;
-
+                userGroupAccesses = getBody().object.userGroupAccesses || [];
+    
             userGroupField.clearValue();
             b.disable();
 

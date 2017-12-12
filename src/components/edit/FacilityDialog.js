d21cb3ec1c942d4e57d2dfc59f10577e5a4ce581
Bjørn Sandvik <bjorn@mastermaps.com>
2017-12-12 16:36:35 +0100

    Facility layer style dialog

--- src/components/edit/FacilityDialog.js
+++ src/components/edit/FacilityDialog.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 import i18next from 'i18next';
 import { Tabs, Tab } from 'd2-ui/lib/tabs/Tabs';
 import TextField from 'd2-ui/lib/text-field/TextField';
+import Checkbox from '../d2-ui/Checkbox';
+import FontStyle from '../d2-ui/FontStyle';
 import OrgUnitGroupSetSelect from '../orgunits/OrgUnitGroupSetSelect';
 import OrgUnitTree from '../orgunits/OrgUnitTree';
 import OrgUnitGroupSelect from '../orgunits/OrgUnitGroupSelect';
 import OrgUnitLevelSelect from '../orgunits/OrgUnitLevelSelect';
 import UserOrgUnitsSelect from '../orgunits/UserOrgUnitsSelect';
-import Checkbox from '../d2-ui/Checkbox';
-import FontStyle from '../d2-ui/FontStyle';
 
 import {
     setOrganisationUnitGroupSet,
